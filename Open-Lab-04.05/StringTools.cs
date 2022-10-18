20c97f18f121a5192dda17b5ae944a46f5d00bc0
Michal Bernát <113421230+michau6666@users.noreply.github.com>
2022-10-18 09:57:24 +0200

    Update StringTools.cs

--- Open-Lab-04.05/StringTools.cs
+++ Open-Lab-04.05/StringTools.cs
@@ -6,7 +6,15 @@ namespace Open_Lab_04._05
     {
         public string Repeat(string orig, int n)
         {
-            throw new NotImplementedException();
+            string result = String.Empty;
+            foreach (char a in orig)
+            {
+                for (int g = 0; g < n; g++)
+                {
+                    result += a;
+                }
+            }
+            return result;
         }
     }
 }
